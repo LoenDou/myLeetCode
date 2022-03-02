ef6c4ff7643dac29b1ae95a9259bcceeb447135d
LeonDou <yiwang_d@163.com>
2022-03-02 21:23:44 +0800

    add daily question.

--- src/Algorithms/LeetCode_0976_Largest_Perimeter_Triangle.java
+++ src/Algorithms/LeetCode_0976_Largest_Perimeter_Triangle.java
@@ -1,2 +1,18 @@
-package Algorithms;public class LeetCode_0976_Largest_Perimeter_Triangle {
+package Algorithms;
+
+import java.util.Arrays;
+
+public class LeetCode_0976_Largest_Perimeter_Triangle {
+    public int largestPerimeter(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        Arrays.sort(nums);
+        for (int i = nums.length - 1; i >= 2 ; i--) {
+            if (nums[i - 2] + nums[i - 1] > nums[i]){
+                return nums[i - 2] + nums[i - 1] + nums[i];
+            }
+        }
+        return 0;
+    }
 }
