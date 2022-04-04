682def8ad70c2fe0efe802bbe301856e2529f71e
LeonDou <yiwang_d@163.com>
2022-04-04 22:52:40 +0800

    add daily question.

---
+++ src/Algorithms/LeetCode_169_Majority_Element.java
@@ -0,0 +1,33 @@
+package Algorithms;
+
+import java.util.HashMap;
+
+public class LeetCode_169_Majority_Element {
+    public int majorityElement1(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                map.put(nums[i], map.get(nums[i]) + 1);
+                if (map.get(nums[i]) > nums.length / 2) {
+                    return nums[i];
+                }
+            } else {
+                map.put(nums[i], 1);
+            }
+        }
+        return nums[0];
+    }
+
+    public int majorityElement2(int[] nums) {
+        int num = 0;
+        int count = 0;
+        for (int i :
+                nums) {
+            if (count == 0) {
+                num = i;
+            }
+            count += num == i ? 1 : -1;
+        }
+        return num;
+    }
+}
